badd9e2f5bacbc22580d8b6b5b102d8e53de5e1a
Mark Pemburn <markpemburn@trak.io>
2023-04-05 15:40:45 -0400

    Begin coding MailEvent class.

--- src/MailEvent.php
+++ src/MailEvent.php
@@ -22,5 +22,34 @@ class MailEvent
 
     public function addActions()
     {
+        add_action('publish_post', [$this, 'onPublishPost']);
+    }
+
+    public function onPublishPost($id, $post)
+    {
+        if ($post->post_type !== 'tribe_event' && $post->post_date !== $post->post_modified) {
+            return;
+        }
+
+
+    }
+
+    protected function parseMessage($contact, $post): string
+    {
+        $message = get_option('mail_event_message');
+        $vars = [
+            '[name]' => $contact['first_name'],
+            '[event_title]' => $post->post_title,
+            '[event_date]' => '2023-04-04 15:30',
+            '[event_permalink]' => $post->post_permalink,
+        ];
+
+        preg_match_all('/\[[\w_]+\]/', $message, $matches);
+        foreach (current($matches) as $match) {
+            $replace = $vars[$match];
+            $message = str_replace($match, $replace, $message);
+        }
+
+        return $message;
     }
 }
